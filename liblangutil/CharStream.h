bc316ddcd63a6b962425fd50442eddff419f3198
Christian Parpart <christian@parpart.family>
2021-03-01 15:07:59 +0100

    liblangUtil: Extends CharStream to translate from line/column to offset.

--- liblangutil/CharStream.h
+++ liblangutil/CharStream.h
@@ -51,6 +51,7 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <tuple>
 #include <utility>
@@ -100,6 +101,14 @@ public:
 	std::string lineAtPosition(int _position) const;
 	LineColumn translatePositionToLineColumn(int _position) const;
 	///@}
+
+	/// Translates a line:column to the absolute position.
+	/// Parameters @p _line and @p _column are 0-based indices.
+	std::optional<int> translateLineColumnToPosition(int _line, int _column) const;
+
+	/// Translates a line:column to the absolute position for the given input text.
+	/// Parameters @p _line and @p _column are 0-based indices.
+	static std::optional<int> translateLineColumnToPosition(std::string const& _text, int _line, int _column);
 
 	/// Tests whether or not given octet sequence is present at the current position in stream.
 	/// @returns true if the sequence could be found, false otherwise.
